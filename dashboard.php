078760e3426b374ab56a1ad920a1991d29f1ee73
softpro-ppm <softpro.ppm@gmail.com>
2025-05-23 12:59:01 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -65,7 +65,7 @@ require_once 'includes/sidebar.php';
               <div class="info-box">
                 <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
                 <div class="info-box-content">
-                  <span class="info-box-text">Total Students  yyyyyy</span>
+                  <span class="info-box-text">Total Students </span>
                   <span class="info-box-number">1,699</span>
                   <span class="info-box-text">+15% from last month</span>
                 </div>
