a4ac7e0633506693cefcecaf832d3499206dbe66
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-05-24 10:57:39 +0530

    Update batches-partner-center.js

--- assets/js/batches-partner-center.js
+++ assets/js/batches-partner-center.js
@@ -2,15 +2,15 @@
 $(document).ready(function() {
     function loadCenters(selectedId) {
         $.ajax({
-            url: 'inc/ajax/training_centers_ajax.php',
+            url: 'inc/ajax/training-centers.php',
             type: 'POST',
-            data: { action: 'read', per_page: 1000 },
+            data: { action: 'list' },
             dataType: 'json',
             success: function(res) {
                 var $center = $('#center_id');
                 $center.empty().append('<option value="">Select Training Center</option>');
-                if(res.data && res.data.data && res.data.data.length) {
-                    $.each(res.data.data, function(i, c) {
+                if(res.data && res.data.length) {
+                    $.each(res.data, function(i, c) {
                         $center.append(`<option value="${c.center_id}"${selectedId==c.center_id?' selected':''}>${c.center_name}</option>`);
                     });
                 }
